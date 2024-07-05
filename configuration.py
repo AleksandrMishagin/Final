b0899a977a98d925799ce4e63e3095f01a4dccb9
AleksandrMishagin <a.mishagin1986@gmail.com>
2024-07-05 13:18:19 +0300

    The second version

--- configuration.py
+++ configuration.py
@@ -1,5 +1,6 @@
 # Мишагин Александр 18-я когорта QA+. Финальный проект.
 
 # заменить URL_OF_ORDERS на действующее
-URL_OF_ORDERS = "https://5e0ab19a-32c6-453c-a6bf-83a63a8f469b.serverhub.praktikum-services.ru"
+URL_OF_ORDERS = "https://b29bb688-80ef-4c24-8a62-3390900eabb5.serverhub.praktikum-services.ru"
 CREATE_ORDER = "/api/v1/orders"
+GET_ORDER = "/api/v1/orders/track?t"
